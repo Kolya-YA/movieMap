4d850078c77d02374be4b7dde357dd0715818b4b
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2024-07-19 02:21:37 +0200

    feat: added Movie Details endpoint

--- server/src/utils/config.ts
+++ server/src/utils/config.ts
@@ -4,7 +4,16 @@ const TMDB_AUTH_TOKEN = process.env.TMDB_AUTH_TOKEN;
 const TMDB_API_KEY = process.env.TMDB_API_KEY;
 const TMDB_URL = process.env.TMDB_API;
 const TMDB_IMAGE_API = process.env.TMDB_IMAGE_API;
-const TMDB_IMAGE_API_THAMNAIL = `${TMDB_IMAGE_API}92`;
+
+const POSTER_SIZES = {
+    w92px: "w92",
+    w154px: "w154",
+    w185px: "w185",
+    w342px: "w342",
+    w500px: "w500",
+    w780px: "w780",
+    original: "original",
+};
 
 // const MONGODB_URI = process.env.MONGODB_URI;
 // const JWT_SECRET = process.env.JWT_SECRET;
@@ -14,4 +23,4 @@ const TMDB_IMAGE_API_THAMNAIL = `${TMDB_IMAGE_API}92`;
     // };
     
 
-export { PORT, TMDB_URL, TMDB_AUTH_TOKEN, TMDB_IMAGE_API, TMDB_API_KEY, TMDB_IMAGE_API_THAMNAIL };
+export { PORT, TMDB_URL, TMDB_AUTH_TOKEN, TMDB_IMAGE_API, TMDB_API_KEY, POSTER_SIZES};
