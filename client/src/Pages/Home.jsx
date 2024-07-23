4ff93164900bbb0248026d1aadc822b87edebbf3
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2024-07-23 16:54:55 +0200

    feat: Refactor Home component, remove unused movie list

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -3,14 +3,6 @@ import axios from 'axios'
 import { UserContext } from '../contexts'
 import { UserHomePage, GuestHomePage } from '../components'
 
-const tempMovieList = [
-    { id: 111, title: 'The Shawshank Redemption', year: 1994, rating: 9.2 },
-    { id: 222, title: 'The Godfather', year: 1972, rating: 9.1 },
-    { id: 333, title: 'The Dark Knight', year: 2008, rating: 9.0 },
-    { id: 444, title: 'The Lord of the Rings: The Return of the King', year: 2003, rating: 8.9 },
-    { id: 555, title: 'Pulp Fiction', year: 1994, rating: 8.9 }
-];
-
 const Home = () => {
     const { user } = useContext(UserContext)    
     const [dailyRecs, setDailyRecs] = useState([])
