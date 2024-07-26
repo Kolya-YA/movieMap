640ceb419f7c48ad4f7f9dfa36f002e65f0c330f
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2024-07-26 21:11:25 +0200

    refactor InputComponent add password show toggler

--- client/src/components/FormComponents/CheckboxComponent.jsx
+++ client/src/components/FormComponents/CheckboxComponent.jsx
@@ -1,21 +1,16 @@
-const Checkbox = ({ id, req, label, onChange, value, icon }) => {
+const Checkbox = ({ id, req, label, onChange, value }) => {
   return (
-    <div className="flex items-center justify-center space-x-3">
+    <div className="flex w-fit px-2 py-1 leading-none focus-within:shadow-diffused hover:shadow-diffused ">
       <input
-        className="w-4 h-4 bg-black bg-opacity-70
-        accent-transparent
-        focus:outline-none
-        focus:shadow-diffused
-        hover:shadow-diffused"
+        className="w-4 h-4 bg-black/70 me-2 accent-transparent focus:outline-none"
         type="checkbox"
-        checked={value}
-        onChange={onChange}
+        checked={value} onChange={onChange}
         id={id}
         required={req}
       />
       <label htmlFor={id}>
         {label}
-        {req && <span className="text-red-500">*</span>}
+        {req && <span className="text-red-300">*</span>}
       </label>
     </div>
   );
--- client/src/components/FormComponents/InputComponent.jsx
+++ client/src/components/FormComponents/InputComponent.jsx
@@ -1,28 +1,34 @@
-const InputComponent = ({ type, id, req, label, onChange, value, placeholder, icon, autocomplete, endButton }) => {
+
+
+const InputComponent = ({ type, id, req, label, onChange, value, placeholder, Icon, autocomplete, PswdToggler, togglePsdwShow, psdwShow }) => {
 
     return (
         <div className="relative">
-            <label htmlFor={id} className="block text-white sr-only">
-                {label}
-                {req && <span className="text-red-500">*</span>}
-            </label>
-            {icon && <span className="absolute flex items-center pl-2 pt-2 ">{icon}</span>}
-            <input className=" ${endButton ? 'pr-10' : ''} w-full px-4 py-2 pl-10 border border-white border-opacity-70 bg-black bg-opacity-70 text-white rounded hover:border-gray-600
-            focus:shadow-diffused focus:outline-none focus:border-gray-600
+            <label htmlFor={id} className="sr-only">{label}</label>
+
+            <input className="
+             ${endButton ? 'pe-10' : ''} w-full pe-4 ps-10 py-2
+             border border-white border-opacity-70 bg-black/70 rounded
+            hover:border-gray-600
+                focus:shadow-diffused focus:outline-none focus:border-gray-600
                  transition duration-300"
                 placeholder={placeholder}
-                autocomplete={autocomplete}
-                type={type}
+                autoComplete={autocomplete}
+                type={psdwShow? 'text' : type}
                 value={value}
                 onChange={onChange}
                 id={id}
                 required={req}
-                // TODO: Add show/hide toggle for password fields
-                // TODO: Add validation attributes
-                // TODO: Add autocomplete attributes
-                //TODO: Style the
+            // TODO: Add validation attributes
+            // TODO: Add autocomplete attributes
             />
-            {endButton && <span className="absolute inset-y-0 right-0 flex items-center pr-3">{endButton}</span>}
+
+            {Icon && <span className="absolute start-2 inset-y-0 block my-auto max-h-fit">
+                {<Icon size={24} aria-hidden="true" />}
+            </span>}
+            {PswdToggler
+                && <PswdToggler psdwShow={psdwShow} togglePsdwShow={togglePsdwShow} />
+            }
         </div>
     )
 }
