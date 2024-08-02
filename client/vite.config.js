15e95b3597ca86c5eaaa27590296a4bfad5553b2
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2024-08-02 19:58:21 +0200

    fix: vit config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,45 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	server: {
-		proxy: {
-			"/api": {
-				// target: "http://localhost:3001",
-				target: "https://moviemapserver.onrender.com",
-				changeOrigin: true,
+// export default defineConfig({
+
+// 	const env = loadEnv(mode, process.cwd(), '')
+
+// 	plugins: [react()],
+// 	server: {
+// 		proxy: {
+// 			"/api": {
+// 				// target: "http://localhost:3001",
+// 				// target: "https://moviemapserver.onrender.com",
+// 				target: env.VITE_API_URL,
+// 				changeOrigin: true,
+// 			},
+// 		},
+// 	},
+// 	define: {
+// 		"process.env.VITE_API_URL": JSON.stringify(env.VITE_API_URL),
+// 	},
+// });
+
+export default defineConfig(({ command, mode }) => {
+	// Load env file based on `mode` in the current working directory.
+	// Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+	const env = loadEnv(mode, process.cwd(), "");
+
+	return {
+		plugins: [react()],
+		server: {
+			proxy: {
+				"/api": {
+					target: env.VITE_API_URL,
+					changeOrigin: true,
+					//   rewrite: (path) => path.replace(/^\/api/, '')
+				},
 			},
 		},
-	},
+		define: {
+			"process.env.VITE_API_URL": JSON.stringify(env.VITE_API_URL),
+		},
+	};
 });
