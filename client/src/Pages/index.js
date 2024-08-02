2a660282e71e712c01bf78793169cdc677da146c
Nikolay Kolomyytsev <1748556+Kolya-YA@users.noreply.github.com>
2024-08-02 18:21:22 +0200

    Revert "Search page"

--- client/src/Pages/index.js
+++ client/src/Pages/index.js
@@ -1,10 +1,19 @@
-import Home from './Home';
-import Login from './Login';
-import SignUp from './Signup';
-import About from './About';
-import SearchMovies from './SearchMovies';
-import Movie from './Movie';
-import UserProfile from './UserProfile';
-import UserMovieList from './UserMovieList';
+import Home from "./Home";
+import Login from "./Login";
+import SignUp from "./Signup";
+import About from "./About";
+import SearchMovies from "./SearchMovies";
+import Movie from "./Movie";
+import UserProfile from "./UserProfile";
+import UserMovieList from "./UserMovieList";
 
-export { Home, Login, SignUp, About, SearchMovies, Movie, UserProfile, UserMovieList };
+export {
+	Home,
+	Login,
+	SignUp,
+	About,
+	SearchMovies,
+	Movie,
+	UserProfile,
+	UserMovieList,
+};
