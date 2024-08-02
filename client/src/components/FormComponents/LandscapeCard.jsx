ef6f1f5e8969244d9a0ddabfbad7fcefbecd4801
janghw33 <janghw33@gmail.com>
2024-08-02 15:37:51 +0200

    useHandler hook

--- client/src/components/FormComponents/LandscapeCard.jsx
+++ client/src/components/FormComponents/LandscapeCard.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { FormatNumber, StarRating } from '../../components';
+import { useTapHandler } from '../../hooks';
 
-function LandscapeCard({ movie }) {
+const LandscapeCard = ({ movie }) => {
+    const cardRef = useTapHandler(movie.id);
+
     return (
-        <Link to={`../movie/${movie.id}`} className="h-40 text-white mb-4 px-4">
+        <div ref={cardRef} className="h-40 text-white mb-4 px-4 cursor-pointer">
             <div className="flex h-40 items-center rounded-xl gap-4 mb-4 px-4 bg-opacity-50 bg-gray-500">
                 <img
                     width="4rem"
@@ -14,21 +16,21 @@ function LandscapeCard({ movie }) {
                     alt={`${movie.title} poster`}
                     loading="lazy"
                 />
-                <div>
+                <div className="bg-black">
                     <h3 className="font-bold line-clamp-1 ">{movie.title}</h3>
                     <p className="text-sm flex gap-2 line-clamp-1">
                         <span className="font-semibold">{movie.release_date.split('-')[0]}</span>
                         <span className="line-clamp-1">{movie.genres_list.join(', ')}</span>
                     </p>
                     <div className="flex gap-2 text-sm ">
-                        <StarRating rating={movie.vote_average} />
+                        <StarRating size={16} rating={movie.vote_average} />
                         (<FormatNumber number={movie.vote_count} />)
                     </div>
                     <p className="text-sm line-clamp-3">{movie.overview}</p>
                 </div>
             </div>
-        </Link>
+        </div>
     );
-}
+};
 
 export default LandscapeCard;
