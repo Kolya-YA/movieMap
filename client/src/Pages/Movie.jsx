11095df6eb32eb2dc001f19d1d9a98c906ed0404
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2024-08-07 03:20:18 +0200

    fix: poster size on movie details page

--- client/src/Pages/Movie.jsx
+++ client/src/Pages/Movie.jsx
@@ -14,14 +14,14 @@ const Movie = () => {
 
     return (
         <div className="text-main-text grid grid-cols-1 grid-rows-[1fr_auto] p-2 font-playfair">
-            <div className="aspect-[2/3]">
+            <div className="">
                 <img
-                    className="rounded-lg"
-                    src={`https://image.tmdb.org/t/p/original/${detail.poster_path}`}
+                    className="mx-auto rounded-lg"
+                    src={`https://image.tmdb.org/t/p/w342/${detail.poster_path}`}
                     alt={detail.title}
                 />
             </div>
-            <div className="grid gap-4 row-start-2 row-span-2 -mt-40 bg-black/70 p-4">
+            <div className="grid gap-4 -mt-40 bg-black/70 p-2">
                 <h1 className="text-3xl text-center text-balance font-semibold font-black-ops-one">{detail.title}</h1>
                 <TechInfo movie={detail} />
                 <Rating movie={detail} />
